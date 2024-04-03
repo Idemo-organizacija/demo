9e32e9336fac463cc0b6d81cc19d10810afb2708
DusanTodorovic5 <dtodorovic40@gmail.com>
2024-04-03 21:43:43 +0200

    feat: landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LandingTopComponent } from './components/landing-top/landing-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbModule, NavbarComponent, LandingTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/landing-top/landing-top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-top',
+  standalone: true,
+  imports: [],
+  templateUrl: './landing-top.component.html',
+  styleUrl: './landing-top.component.css'
+})
+export class LandingTopComponent {
+
+}
